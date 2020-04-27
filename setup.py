d1966efc43de7ab4ac06d4a3307e5efd3d0fd6b3
Thomas G <thogar@noc.ac.uk>
2020-04-27 16:33:21 +0100

    type change to dic and version number increased

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name="COAsT",
-    version="0.1",
+    version="0.1.1",
     description="This is the Coastal Ocean Assessment Tool",
     url="https://www.bodc.ac.uk",
-    download_url = 'https://github.com/British-Oceanographic-Data-Centre/COAsT/archive/0.1.tar.gz',
+    download_url = 'https://github.com/British-Oceanographic-Data-Centre/COAsT/archive/0.1.1.tar.gz',
     author="British Oceanographic Data Centre (BODC)",
     author_email="bodcsoft@bodc.ac.uk",
     license="Put something here",  # TODO,
@@ -18,7 +18,7 @@ setup(
         "Programming Language :: Python :: 3.7",
     ],
     keywords=["NEMO", "shallow water", "ocean assessment"],
-    project_urls=["https://british-oceanographic-data-centre.github.io/COAsT/"],
+    project_urls={"documentation":"https://british-oceanographic-data-centre.github.io/COAsT/"},
     install_requires=[
         'dask',
         'xarray',
