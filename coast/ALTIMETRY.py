bfda689973ae10ae3db248ca2a16eb922f10d900
davbyr <dbyrne@noc.ac.uk>
2020-06-18 15:50:14 +0100

    Added renaming functionality and quick_plot() functionality in ALTIMETRY

--- coast/ALTIMETRY.py
+++ coast/ALTIMETRY.py
@@ -4,7 +4,6 @@ from warnings import warn
 import numpy as np
 import xarray as xa
 
-
 class ALTIMETRY(OBSERVATION):
 
     def __init__(self):
@@ -41,3 +40,41 @@ class ALTIMETRY(OBSERVATION):
                 warn(str(e))
                 
         self.adjust_longitudes()
+        
+    def quick_plot(self, var: str=None):
+        try:
+            import cartopy.crs as ccrs  # mapping plots
+            import cartopy.feature  # add rivers, regional boundaries etc
+            from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER  # deg symb
+            from cartopy.feature import NaturalEarthFeature  # fine resolution coastline
+        except ImportError:
+            import sys
+            warn("No cartopy found - please run\nconda install -c conda-forge cartopy")
+            sys.exit(-1)
+            
+        import matplotlib.pyplot as plt
+        fig = plt.figure(figsize=(10, 10))
+        ax = fig.gca()
+        ax = plt.subplot(1, 1, 1, projection=ccrs.PlateCarree())
+        
+        if var is None:
+            cset = self.dataset.plot.scatter(x='longitude',y='latitude')
+        else:
+            cset = self.dataset.plot.scatter(x='longitude',y='latitude',hue=var)
+
+        ax.add_feature(cartopy.feature.BORDERS, linestyle=':')
+        coast = NaturalEarthFeature(category='physical', scale='50m', facecolor='none', name='coastline')
+        ax.add_feature(coast, edgecolor='gray')
+
+        gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
+                          linewidth=0.5, color='gray', alpha=0.5, linestyle='-')
+
+        gl.xlabels_top = False
+        gl.xlabels_bottom = True
+        gl.ylabels_right = False
+        gl.ylabels_left = True
+        gl.xformatter = LONGITUDE_FORMATTER
+        gl.yformatter = LATITUDE_FORMATTER
+
+        plt.show()
+        return
