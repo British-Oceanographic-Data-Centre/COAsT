79684c7c770a007939db532edacc4cd448134642
davbyr <dbyrne@noc.ac.uk>
2020-06-22 17:31:53 +0100

    Objectified CRPS

--- coast/CRPS.py
+++ coast/CRPS.py
@@ -1,7 +1,6 @@
 import numpy as np
 import xarray as xa
 from warnings import warn
-import matplotlib.pyplot as plt
 from .CDF import CDF
 from .interpolate_along_dimension import interpolate_along_dimension
 
@@ -18,49 +17,20 @@ class CRPS():
     Example basic usage::
         
         $ crps_obj = coast.CRPS(nemo_data, nemo_domain, altimetry)
-        $ crps_obj.calculate('sossheig', 'sla_filtered', nh_radius=111)
         $ crps_list = crps_obj.crps # Get crps values
         $ crps.map_plot() # Plots CRPS on map
     '''
     
-    def __init__(self, mod_data, mod_dom, obs_object):
+    def __init__(self, mod_data, mod_dom, obs_object, 
+                 mod_var:str, obs_var:str,
+                 nh_radius: float=111, nh_type: str="radius", 
+                 cdf_type: str="empirical", time_interp:str="nearest"):
         """Initialisation of CRPS object.
 
         Args:
             mod_data (COAsT): COAsT model data object.
             mod_dom  (COAsT): COAsT model domain object.
             obs_object (OBSERVATION): COAsT OBSERVATION object.
-
-        Returns:
-            CRPS: Returns a new instance of a CRPS object.
-        """
-        
-        self.mod_data    = mod_data
-        self.mod_dom     = mod_dom
-        self.obs_object  = obs_object
-        self.nh_radius   = None
-        self.nh_type     = None
-        self.cdf_type    = None
-        self.time_interp = None
-        self.crps        = None
-        self.mod_var     = None
-        self.obs_var     = None
-        self.n_model_pts = None
-        self.contains_land = None
-        self.longitude   = obs_object['longitude']
-        self.latitude    = obs_object['latitude']
-        return
-        
-    def __getitem__(self, varstr:str):
-        """Gets variable from __dict__"""
-        return self.__dict__[varstr]
-
-    def calculate(self, mod_var: str, obs_var: str, nh_radius: float=111, 
-                  nh_type: str="radius", cdf_type: str="empirical", 
-                  time_interp:str="nearest"):
-        """Calculate CRPS values for specified variables/methods/radii.
-
-        Args:
             mod_var (str): Name of variable to use from model object.
             obs_var (str): Name of variable to use from observation object.
             nh_radius(float): Neighbourhood radius.
@@ -69,77 +39,50 @@ class CRPS():
                            Either 'empirical' or 'theoretical'
             time_interp(str): Type of time interpolation.
                               Either 'nearest' or 'linear'
+
         Returns:
-            array: CRPS values.
+            CRPS: Returns a new instance of a CRPS object.
         """
-        self.mod_var = mod_var
-        self.obs_var = obs_var
+        #Input variables
+        self.mod_data    = mod_data
+        self.mod_dom     = mod_dom
+        self.obs_object  = obs_object
         self.nh_radius   = nh_radius
         self.nh_type     = nh_type
         self.cdf_type    = cdf_type
         self.time_interp = time_interp
-        tmp = self.calculate_sonf()
+        self.mod_var     = mod_var
+        self.obs_var     = obs_var
+        self.longitude   = obs_object['longitude']
+        self.latitude    = obs_object['latitude']
+        # Output variables
+        self.crps        = None
+        self.n_model_pts = None
+        self.contains_land = None
+        self.mean = None
+        self.mean_noland = None
+        self.calculate()
+        return
+    
+    def __getitem__(self, varstr:str):
+        """Gets variable from __dict__"""
+        return self.__dict__[varstr]
+
+    def calculate(self):
+        """Calculate CRPS values for specified variables/methods/radii."""
+        tmp = self.calculate_sonf(self.mod_data[self.mod_var],
+                                  self.mod_dom, self.obs_object[self.obs_var],
+                                  self.nh_radius, self.nh_type, self.cdf_type,
+                                  self.time_interp)
         self.crps = tmp[0]
         self.n_model_pts = tmp[1]
         self.contains_land = tmp[2]
+        self.mean = np.nanmean(tmp[0])
+        self.mean_noland = np.nanmean(tmp[0][tmp[2]==0])
         return 
     
-    def cdf_plot(self, index):
-        index = [index]
-        tmp = self.calculate_sonf(index)
-        crps_tmp = tmp[0]
-        n_mod_pts = tmp[1]
-        contains_land = tmp[2]
-        mod_cdf = tmp[3]
-        obs_cdf = tmp[4]
-        fig = plt.figure()
-        ax = plt.subplot(111)
-        ax.plot(mod_cdf.disc_x, mod_cdf.disc_y, c='k', 
-                linestyle='--')
-        ax.plot(obs_cdf.disc_x, obs_cdf.disc_y, linestyle='--')
-        ax.fill_between(mod_cdf.disc_x, mod_cdf.disc_y, 
-                        obs_cdf.disc_y, alpha=0.5)
-        titlestr = 'CRPS = ' + str(round( crps_tmp[index[0]], 3)) + '\n'
-        titlestr = titlestr + '# Model Points : ' + str(n_mod_pts[0]) + '  |  '
-        titlestr = titlestr + 'Contains land : ' + str(bool(contains_land[0])) 
-        plt.title(titlestr)
-        plt.legend(['Model', 'Observations'])
-        return fig, ax
-    
-    def map_plot(self, crps_var: str='crps'):
-        try:
-            import cartopy.crs as ccrs  # mapping plots
-            import cartopy.feature  # add rivers, regional boundaries etc
-            from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER  # deg symb
-            from cartopy.feature import NaturalEarthFeature  # fine resolution coastline
-            import matplotlib.pyplot as plt
-        except ImportError:
-            import sys
-            warn("No cartopy found - please run\nconda install -c conda-forge cartopy")
-            sys.exit(-1)
-            
-        fig = plt.figure(figsize=(10, 10))
-        ax = fig.gca()
-        ax = plt.subplot(1, 1, 1, projection=ccrs.PlateCarree())
-        plt.scatter(self.longitude, self.latitude, c=self[crps_var])
-        plt.colorbar()
-        plt.title('Continuous Rank Probability Score')
-        ax.add_feature(cartopy.feature.BORDERS, linestyle=':')
-        coast = NaturalEarthFeature(category='physical', scale='50m', 
-                                    facecolor='none', name='coastline')
-        ax.add_feature(coast, edgecolor='gray')
-        gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
-                          linewidth=0.5, color='gray', alpha=0.5, linestyle='-')
-        gl.xlabels_top = False
-        gl.xlabels_bottom = True
-        gl.ylabels_right = False
-        gl.ylabels_left = True
-        gl.xformatter = LONGITUDE_FORMATTER
-        gl.yformatter = LATITUDE_FORMATTER
-        plt.show()
-        return fig, ax
-    
-    def calculate_sonf(self, nh_indices=None):
+    def calculate_sonf(self, mod_var, mod_dom, obs_var, 
+                       nh_radius, nh_type, cdf_type, time_interp):
         """Calculatues the Continuous Ranked Probability Score (CRPS)
     
         Calculatues the Continuous Ranked Probability Score (CRPS) using
@@ -157,37 +100,26 @@ class CRPS():
         
         # Define var_dict to determine which variable to use and define some
         # function variables
-        mod_data     = self.mod_data
-        obs          = self.obs_object
-        mod_dom      = self.mod_dom
-        time_interp  = self.time_interp
-        nh_type      = self.nh_type
-        nh_radius    = self.nh_radius
-        cdf_type     = self.cdf_type
-        mod_var      = self.mod_var
-        obs_var      = self.obs_var
-        if len(mod_data[mod_var].dims) > 3:
+        if len(mod_var.dims) > 3:
             raise Exception('COAsT: CRPS Input data must only have dims ' + 
                             '(time, lon, lat)')
     
         # Define output array and check for scalars being used as observations.
         # If so, put obs into lists/arrays.
-        n_nh = obs[obs_var].shape[0] # Number of neighbourhoods (n_nh)  
+        n_nh = obs_var.shape[0] # Number of neighbourhoods (n_nh)  
         crps_list = np.zeros( n_nh )*np.nan
         n_model_pts = np.zeros( n_nh )*np.nan
         contains_land = np.zeros( n_nh , dtype=bool)
         
         # Time interpolation weights object
-        weights = interpolate_along_dimension(mod_data[mod_var], 
-                                              obs['time'], 'time_counter', 
+        weights = interpolate_along_dimension(mod_var, 
+                                              obs_var['time'], 'time_counter', 
                                               method = time_interp)
         
-        if nh_indices is None:
-            nh_indices = np.arange(0,n_nh)
+        nh_indices = np.arange(0,n_nh)
         
         # Loop over neighbourhoods
         for ii in nh_indices:
-        
             # Neighbourhood centre
             cntr_lon = self.longitude[ii]
             cntr_lat = self.latitude[ii]
@@ -197,7 +129,7 @@ class CRPS():
                 subset_indices = mod_dom.subset_indices_by_distance(cntr_lon, 
                                  cntr_lat, nh_radius)
             elif nh_type == "box":
-                pass
+                raise NotImplementedError
             
             # Subset model data in time and space: What is the model doing at
             # observation times?
@@ -211,11 +143,88 @@ class CRPS():
             else:
                 # Create model and observation CDF objects
                 mod_cdf = CDF(mod_subset, cdf_type=cdf_type)
-                obs_cdf = CDF(obs[obs_var][ii], cdf_type='empirical')
+                obs_cdf = CDF([obs_var[ii]], cdf_type='empirical')
                 
                 # Calculate CRPS and put into output array
-                crps_list[ii] = mod_cdf.difference(obs_cdf)
-                n_model_pts[ii] = mod_cdf.sample_size[0]
+                crps_list[ii] = mod_cdf.crps(obs_var[ii])
+                n_model_pts[ii] = int(mod_subset.shape[0])
 
         return crps_list, n_model_pts, contains_land, mod_cdf, obs_cdf
+    
+    def cdf_plot(self, index):
+        """A comparison plot of the model and observation CDFs.
+
+        Args:
+            index (int): Observation index to plot CDFs for (single index).
+
+        Returns:
+            Figure and axes objects for the resulting image.
+        """
+        index=[index]
+        tmp = self.calculate_sonf(self.mod_data[self.mod_var],
+                                  self.mod_dom, 
+                                  self.obs_object[self.obs_var][index],
+                                  self.nh_radius, self.nh_type, self.cdf_type,
+                                  self.time_interp)
+        crps_tmp = tmp[0]
+        n_mod_pts = tmp[1]
+        contains_land = tmp[2]
+        mod_cdf = tmp[3]
+        obs_cdf = tmp[4]
+        fig, ax = mod_cdf.diff_plot(obs_cdf)
+        titlestr = 'CRPS = ' + str(round( crps_tmp[0], 3)) + '\n'
+        titlestr = titlestr + '# Model Points : ' + str(n_mod_pts[0]) + '  |  '
+        titlestr = titlestr + 'Contains land : ' + str(bool(contains_land[0])) 
+        ax.set_title(titlestr)
+        ax.grid()
+        ax.legend(['Model', 'Observations'])
+        return fig, ax
+    
+    def map_plot(self, stats_var: str='crps'):
+        """Plots CRPS (or other variables) at observation locations on a map
+
+        Args:
+            stats_var (str): Either 'crps', 'n_model_pts' or 'contains_land'.
+
+        Returns:
+            Figure and axes objects for the resulting image.
+        """
+        try:
+            import cartopy.crs as ccrs  # mapping plots
+            import cartopy.feature  # add rivers, regional boundaries etc
+            from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER  # deg symb
+            from cartopy.feature import NaturalEarthFeature  # fine resolution coastline
+            import matplotlib.pyplot as plt
+        except ImportError:
+            import sys
+            warn("No cartopy found - please run\nconda install -c conda-forge cartopy")
+            sys.exit(-1)
+            
+        fig = plt.figure(figsize=(10, 10))
+        ax = fig.gca()
+        ax = plt.subplot(1, 1, 1, projection=ccrs.PlateCarree())
+        plt.scatter(self.longitude, self.latitude, c=self[stats_var])
+        plt.colorbar()
+        ax.add_feature(cartopy.feature.BORDERS, linestyle=':')
+        coast = NaturalEarthFeature(category='physical', scale='50m', 
+                                    facecolor='none', name='coastline')
+        ax.add_feature(coast, edgecolor='gray')
+        gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
+                          linewidth=0.5, color='gray', alpha=0.5, linestyle='-')
+        gl.xlabels_top = False
+        gl.xlabels_bottom = True
+        gl.ylabels_right = False
+        gl.ylabels_left = True
+        gl.xformatter = LONGITUDE_FORMATTER
+        gl.yformatter = LATITUDE_FORMATTER
+        title_dict = {'crps': 'Continuous Rank Probability Score',
+                      'n_model_pts' : 'Number of Model Points Used',
+                      'contains_land': 'CRPS Values that contain land'}
+        try:
+            plt.title(title_dict[stats_var])
+        except:
+            pass
+        
+        plt.show()
+        return fig, ax
     
