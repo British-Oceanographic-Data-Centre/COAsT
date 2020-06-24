93d627615fb53a38851286550e021b08960bd796
davbyr <dbyrne@noc.ac.uk>
2020-06-24 11:36:17 +0100

    Set all renaming dictionaries to None

--- coast/NEMO.py
+++ coast/NEMO.py
@@ -14,11 +14,15 @@ class NEMO(COAsT):
         return
     
     def set_dimension_mapping(self):
-        self.dim_mapping = {'time_counter':'t_dim', 'deptht':'z_dim', 
-                            'y':'y_dim', 'x':'x_dim'}
+        #self.dim_mapping = {'time_counter':'t_dim', 'deptht':'z_dim', 
+        #                    'y':'y_dim', 'x':'x_dim'}
+        self.dim_mapping = None
         
     def set_variable_mapping(self):
-        self.var_mapping = {'time_counter':'time'}
+        #self.var_mapping = {'time_counter':'time',
+        #                    'votemper' : 'temperature',
+        #                    'temp' : 'temperature'}
+        self.var_mapping = None
 
     def get_contour_complex(self, var, points_x, points_y, points_z, tolerance: int = 0.2):
         smaller = self.dataset[var].sel(z=points_z, x=points_x, y=points_y, method='nearest', tolerance=tolerance)
