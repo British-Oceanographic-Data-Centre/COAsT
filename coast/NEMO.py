5e85daea49bdedd3b7364196200842e5aeb8dd35
Anthony Wise <anwise@noc.ac.uk>
2020-06-24 19:03:03 +0100

    Transect fluxes are being calculated and appear to be in right ballpark. Need to test and tidy.

--- coast/NEMO.py
+++ coast/NEMO.py
@@ -27,7 +27,8 @@ class NEMO(COAsT):
         return smaller
     
 class Transect:
-    def __init__(self, dataset: xa.Dataset, domain: COAsT, point_A: tuple, point_B: tuple):
+    def __init__(self, domain: COAsT, point_A: tuple, point_B: tuple,
+                 dataset_T: COAsT=None, dataset_U: COAsT=None, dataset_V: COAsT=None):
         
         self.point_A = point_A
         self.point_B = point_B
@@ -38,8 +39,8 @@ class Transect:
         
         # Redefine transect to be defined as a 'sawtooth' transect, i.e. each point on
         # the transect is seperated from its neighbours by a single index change in y or x, but not both
-        dist_option_1 = domain.e2f.values[0, tran_y, tran_x] + domain.e1f.values[0, tran_y+1, tran_x]
-        dist_option_2 = domain.e2f.values[0, tran_y, tran_x+1] + domain.e1f.values[0, tran_y, tran_x]
+        dist_option_1 = domain.dataset.e2f.values[0, tran_y, tran_x] + domain.dataset.e1f.values[0, tran_y+1, tran_x]
+        dist_option_2 = domain.dataset.e2f.values[0, tran_y, tran_x+1] + domain.dataset.e1f.values[0, tran_y, tran_x]
         spacing = np.abs( np.diff(tran_y) ) + np.abs( np.diff(tran_x) )
         spacing[spacing!=2]=0
         doublespacing = np.nonzero( spacing )[0]
@@ -57,21 +58,24 @@ class Transect:
         self.flux_across_AB = None # (time,depth_index,transect_segment_index)
         self.vol_flux_across_AB = None # (time,transect_segment_index)
         # dataset along the transect
-        da_tran_y = xa.DataArray( tran_y, dims=['a'])
-        da_tran_x = xa.DataArray( tran_x, dims=['a'])
-        self.data = dataset.isel(y=da_tran_y, x=da_tran_x)
+        da_tran_y = xa.DataArray( tran_y, dims=['transect_dim'])
+        da_tran_x = xa.DataArray( tran_x, dims=['transect_dim'])
+        self.data_T = dataset_T.dataset.isel(y=da_tran_y, x=da_tran_x)
+        self.data_U = dataset_U.dataset.isel(y=da_tran_y, x=da_tran_x)
+        self.data_V = dataset_V.dataset.isel(y=da_tran_y, x=da_tran_x)
         # For calculations we need access to a halo of points around the transect
         # self.data_n = dataset.isel(y=tran_y+1,x=tran_x)  
         # self.data_e = dataset.isel(y=tran_y,x=tran_x+1) 
         # self.data_s = dataset.isel(y=tran_y-1,x=tran_x) 
         # self.data_w = dataset.isel(y=tran_y,x=tran_x-1) 
-        self.domain = domain.dataset 
+        self.domain = domain.dataset.isel(y=da_tran_y, x=da_tran_x) 
         # For calculations we need access to a halo of points around the transect
         # self.domain_n = domain.dataset.isel(y=tran_y+1,x=tran_x)  
         # self.domain_e = domain.dataset.isel(y=tran_y,x=tran_x+1) 
         # self.domain_s = domain.dataset.isel(y=tran_y-1,x=tran_x) 
         # self.domain_w = domain.dataset.isel(y=tran_y,x=tran_x-1) 
         
+    
     def transport_across_AB(self):
         
         flux = []
@@ -79,25 +83,38 @@ class Transect:
         dy = np.diff(self.y_idx)
         dx = np.diff(self.x_idx)
         # Loop through each point along the transact
-        for idx in np.arange(0, self.len):
+        for idx in np.arange(0, self.len-1):
             
             if dy[idx] > 0:
                 # u flux (+ in)
-                u_flux = self.data.uoce.values[:,:,idx+1] * self.domain.e2u.values[:,:,idx+1]
+                u_flux = self.data_U.vozocrtx[:,:,idx+1].to_masked_array() * self.domain.e2u[0,idx+1].to_masked_array()
                 flux.append( u_flux )
-                vol_flux.append( np.sum( u_flux * self.data.e3u.values[:,:,idx+1], axis=1 ) )
+                vol_flux.append( np.sum( u_flux * self.data_U.e3u[:,:,idx+1].to_masked_array(), axis=1 ) )
             elif dx[idx] > 0:
                 # v flux (- in)
-                v_flux = -self.data.voce.values[:,:,idx+1] * self.domain.e1v.values[:,:,idx+1]
+                v_flux = -self.data_V.vomecrty[:,:,idx+1].to_masked_array() * self.domain.e1v[0,idx+1].to_masked_array()
                 flux.append( v_flux )
-                vol_flux.append( np.sum( v_flux * self.data.e3v.values[:,:,idx+1], axis=1 ) )             
+                vol_flux.append( np.sum( v_flux * self.data_V.e3v[:,:,idx+1].to_masked_array(), axis=1 ) )             
             elif dx[idx] < 0:
                 # v flux (+ in)
-                v_flux = self.data.voce.values[:,:,idx] * self.domain.e1v.values[:,:,idx]
+                v_flux = self.data_V.vomecrty[:,:,idx].to_masked_array() * self.domain.e1v[0,idx].to_masked_array()
                 flux.append( v_flux )
-                vol_flux.append( np.sum( v_flux * self.data.e3v.values[:,:,idx], axis=1 ) )
+                vol_flux.append( np.sum( v_flux * self.data_V.e3v[:,:,idx].to_masked_array(), axis=1 ) )
         
-        self.flux_across_AB = np.asarray(flux)
-        self.vol_flux_across_AB = np.asarray(vol_flux)
+        self.flux_across_AB = np.ma.asarray(flux)
+        self.vol_flux_across_AB = np.ma.asarray(vol_flux)
         return ( np.sum(self.flux_across_AB, axis=2), np.sum(self.vol_flux_across_AB, axis=1) )  
     
+    def moving_average(a, n=3) :
+    ret = np.cumsum(a, dtype=float, axis=0)
+    ret[n:,:] = ret[n:,:] - ret[:-n,:]
+    return ret[n - 1:,:] / n
+
+    def moving_average(x, w, axis=-1):
+        return np.convolve(x, np.ones(w), axis=axis) / w
+    
+    flux_slice = moving_average(tran.flux_across_AB[:,0,:],1,axis=0)
+zz,ss = np.meshgrid(np.cumsum(tran.domain.e3f_0[0,:,0]), np.arange(0,np.shape(flux_slice)[0]))
+fig,ax=plt.subplots()
+ax.pcolormesh( ss,zz, flux_slice)
+plt.gca().invert_yaxis()
