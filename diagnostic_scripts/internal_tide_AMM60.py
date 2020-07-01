9a16cc56e6e8b267f4df5a89a6705a5851ea00f5
jpolton <jelt@noc.ac.uk>
2020-07-01 14:12:57 +0100

    application to AMM60 data

--- diagnostic_scripts/internal_tide_AMM60.py
+++ diagnostic_scripts/internal_tide_AMM60.py
@@ -25,37 +25,9 @@ fn_nemo_dat = 'COAsT_example_NEMO_data.nc'
 fn_nemo_dom = 'COAsT_example_NEMO_domain.nc'
 #fn_altimetry = 'COAsT_example_altimetry_data.nc'
 
-
-
 #%%
 
-#################################################
-## Loading and initialising methods ##
-#################################################
-
-#sci = coast.NEMO(dir + fn_nemo_dat)
-#dom = coast.DOMAIN(dir + fn_nemo_dom)
-##alt = coast.ALTIMETRY(dir + fn_altimetry)
-
-#sci = coast.NEMO("/projectsa/COAsT/ind_1h_20180101_20160109_shelftmb_grid_T.nc")
-#dom = coast.DOMAIN("/projectsa/accord/BoBEAS/INPUTS/domain_cfg.nc")
-
-#sci = coast.NEMO("/projectsa/NEMO/jelt/AMM60_ARCHER_DUMP/AMM60smago/EXP_NSea/OUTPUT/AMM60_1d_20100130_20100203_grid_T.nc")
-sci = coast.NEMO("/projectsa/NEMO/jelt/AMM60_ARCHER_DUMP/AMM60smago/EXP_NSea/OUTPUT/AMM60_1d_20100704_20100708_grid_T.nc") 
-dom = coast.DOMAIN("/projectsa/FASTNEt/jelt/AMM60/mesh_mask.nc")
-
-#################################################
-## subset of data and domain ##
-#################################################
-# Pick out a North Sea subdomain
-ind = dom.subset_indices([51,-4], [62,10])
-
-#ind = dom.subset_indices([10,90], [15,95]) # Andermann Shelf
-
-dom_nwes = dom.isel(y_dim=ind[0], x_dim=ind[1]) #nwes = northwest europe shelf
-
-
-#%%
+# Copy domain subsetting method to NEMO class
 if(1):
 # Add subset method to NEMO class
     def subset_indices(self, start: tuple, end: tuple, grid_ref: str = 'T') -> tuple:
@@ -100,7 +72,37 @@ if(1):
     setattr(coast.NEMO, 'subset_indices', subset_indices)
     setattr(coast.NEMO, 'find_j_i', find_j_i)
 
-ind_sci = sci.subset_indices([51,-4], [62,10])
+#%%
+
+#################################################
+## Loading and initialising methods ##
+#################################################
+
+#sci = coast.NEMO(dir + fn_nemo_dat)
+#dom = coast.DOMAIN(dir + fn_nemo_dom)
+##alt = coast.ALTIMETRY(dir + fn_altimetry)
+
+#sci = coast.NEMO("/projectsa/COAsT/ind_1h_20180101_20160109_shelftmb_grid_T.nc")
+#dom = coast.DOMAIN("/projectsa/accord/BoBEAS/INPUTS/domain_cfg.nc")
+
+#sci = coast.NEMO("/projectsa/NEMO/jelt/AMM60_ARCHER_DUMP/AMM60smago/EXP_NSea/OUTPUT/AMM60_1d_20100130_20100203_grid_T.nc")
+sci = coast.NEMO("/projectsa/NEMO/jelt/AMM60_ARCHER_DUMP/AMM60smago/EXP_NSea/OUTPUT/AMM60_1d_20100704_20100708_grid_T.nc") 
+dom = coast.DOMAIN("/projectsa/FASTNEt/jelt/AMM60/mesh_mask.nc")
+
+#################################################
+## subset of data and domain ##
+#################################################
+# Pick out a North Sea subdomain
+ind = dom.subset_indices([51,-4], [62,15])
+
+#ind = dom.subset_indices([10,90], [15,95]) # Andermann Shelf
+
+dom_nwes = dom.isel(y_dim=ind[0], x_dim=ind[1]) #nwes = northwest europe shelf
+
+
+#%%
+
+ind_sci = sci.subset_indices([51,-4], [62,15])
 
 sci_nwes = sci.isel(y_dim=ind_sci[0], x_dim=ind_sci[1]) #nwes = northwest europe shelf
 
@@ -125,6 +127,7 @@ IT_obj.get_pyc_vars() # --> self.zd and self.zt
 ## Make Plots
 #################################################
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors # colormap fiddling
 
 #%%
 
@@ -133,7 +136,7 @@ import matplotlib.pyplot as plt
 # short cuts for variable names
 lon =  IT_obj.domain.dataset.glamt.squeeze()
 lat =  IT_obj.domain.dataset.gphit.squeeze()
-dep =  IT_obj.depth_t[:,:,:]
+dep =  IT_obj.domain.depth_t[:,:,:]
 
 fig = plt.figure()
 plt.rcParams['figure.figsize'] = 8,8
@@ -197,6 +200,65 @@ plt.gca().invert_yaxis()
 plt.colorbar()
 plt.show()
 
+#%%
+
+# Overlay contour
+# Extract the coord and depth as a 1D lines
+lat_lin =  dom_nwes.get_subset_as_xarray("gphit",xi,yi).squeeze() 
+zd_lin = IT_obj.get_subset_as_xarray
+
+
+#%%
+
+## Make plot of pycnocline depth and stratification
+
+def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
+    new_cmap = colors.LinearSegmentedColormap.from_list(
+        'trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=minval, b=maxval),
+        cmap(np.linspace(minval, maxval, n)))
+    return new_cmap
+
+cmap = plt.get_cmap('BrBG_r')
+new_cmap = truncate_colormap(cmap, 0.2, 0.8)
+
+import numpy.ma as ma
+
+H = IT_obj.domain.depth_t[-1,:,:].squeeze()
+zd = IT_obj.zd.mean(dim='t_dim')
+
+fig = plt.figure()
+plt.rcParams['figure.figsize'] = (8.0, 8.0)
+
+
+spacing = 15
+
+ax = fig.add_subplot(111)
+#clim = [-10**-6,10**-6]
+cz = plt.contour( lon, lat, H, levels=[11, 80, 200, 800],
+            colors=['k','k','k', 'k'],
+            linewidths=[1,2,1,1])
+plt.contourf( lon, lat, ma.masked_where(H<11, zd),
+             levels=np.arange(0,40.+10.,10.),
+             extend='max',
+             cmap=new_cmap)
+plt.clim([0,40.])
+
+
+plt.xlim([-3,11])
+plt.ylim([51,62])
+plt.colorbar()
+
+
+lines = [cz.collections[i] for i in range(1,len(cz.collections))] #[ cz.collections[1], cz.collections[2], cz.collections[-1] ]
+labels = [str(int(cz.levels[i]))+'m' for i in range(1,len(cz.levels))]
+#labels = ['80m','200m','800m']
+
+plt.legend( lines, labels, loc="lower right")
+
+plt.title('Pycnocline depth (m)')
+plt.xlabel('longitude'); plt.ylabel('latitude')
+
+#fig.savefig(fig_dir+'Upper200mVel.png', dpi=120)
 #%%
 def main():
     pass
