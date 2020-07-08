e93834144b41844f5bb084e54068fbbf3a4ac6b9
Anthony Wise <anwise@noc.ac.uk>
2020-07-08 17:59:45 +0100

    1. Added the option to create a NEMO object with no data file. This allows the user to just pass in the domain path if they need to.
    2. Modified the depth calculation so that now depth_0 is calculated for only the relevent grid and is assigned to NEMO.dataset.depth_0 so that it is used as a coordinate variable. Only implemented for the t and w grids so far.

---
+++ example_scripts/awise_script_for_branch104_fix.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  8 12:53:20 2020
+
+@author: anwise
+"""
+
+import coast
+import numpy as np
+import xarray as xr
+
+NEMO_PATH = 'example_files/nemo_data_'
+DOMAIN_PATH = 'example_files/COAsT_example_NEMO_domain.nc'
+
+ds_t = xr.open_dataset( NEMO_PATH + 'T_grid.nc' )
+ds_u = xr.open_dataset( NEMO_PATH + 'U_grid.nc' )
+ds_v = xr.open_dataset( NEMO_PATH + 'V_grid.nc' )
+
+nemo_t = coast.NEMO( NEMO_PATH + 'T_grid.nc', DOMAIN_PATH, grid_ref='t-grid' )
+nemo_u = coast.NEMO( NEMO_PATH + 'U_grid.nc', DOMAIN_PATH, grid_ref='u-grid' )
+nemo_v = coast.NEMO( NEMO_PATH + 'V_grid.nc', DOMAIN_PATH, grid_ref='v-grid' )
+nemo_f = coast.NEMO( DOMAIN_PATH, grid_ref='f-grid' )
+
+nemo_t.dataset
+nemo_f_dataset
