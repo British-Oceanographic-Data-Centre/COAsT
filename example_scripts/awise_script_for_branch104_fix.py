7f9cca95803cea01d17c22d1a71140587ed5924b
Thomas G <thogar@noc.ac.uk>
2020-09-10 12:06:35 +0100

    files removed and scripts update

--- example_scripts/awise_script_for_branch104_fix.py
+++ example_scripts/awise_script_for_branch104_fix.py
@@ -7,6 +7,7 @@ Created on Wed Jul  8 12:53:20 2020
 """
 
 import coast
+import os
 import numpy as np
 import xarray as xr
 
@@ -18,6 +19,12 @@ fn_nemo_grid_v_dat = 'nemo_data_V_grid.nc'
 fn_nemo_dat = 'COAsT_example_NEMO_data.nc'
 fn_nemo_dom = 'COAsT_example_NEMO_domain.nc'
 fn_altimetry = 'COAsT_example_altimetry_data.nc'
+
+if not os.path.isdir(dn_files):
+    print("please go download the examples file from https://dev.linkedsystems.uk/erddap/files/COAsT_example_files/")
+    dn_files = input("what is the path to the example files:\n")
+    if not os.path.isdir(dn_files):
+        print(f"location f{dn_files} cannot be found")
 
 nemo_t = coast.NEMO( fn_data=dn_files+fn_nemo_grid_t_dat, 
                     fn_domain=dn_files+fn_nemo_dom, grid_ref='t-grid' )
