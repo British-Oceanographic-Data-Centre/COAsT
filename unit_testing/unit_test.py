5cf5180316a42fa3d16155192bbbdfe29a6d6445
jpolton <jelt@noc.ac.uk>
2020-10-16 17:39:06 +0100

    Add unit testing

--- unit_testing/unit_test.py
+++ unit_testing/unit_test.py
@@ -1032,6 +1032,35 @@ except:
     print(str(sec) + chr(subsec) +' FAILED.')
 
 plt.close('all')
+
+#-----------------------------------------------------------------------------#
+#%% ( 7i ) TIDEGAUGE method for tabulated data                                #
+#                                                                             #
+subsec = subsec+1
+
+# Take a look at the sea level time series stored within the object:
+try:
+    filnam = 'example_files/Gladstone_2020-10_HLW.txt'
+    date_start = np.datetime64('2020-10-11 07:59')
+    date_end = np.datetime64('2020-10-20 20:21')
+
+    # Initiate a TIDEGAUGE object, if a filename is passed it assumes it is a GESLA type object
+    tg = coast.TIDEGAUGE()
+    tg.dataset = tg.read_HLW_to_xarray(filnam, date_start, date_end)
+
+    check1 = len(tg.dataset.sea_level) == 37
+    check2 = tg.get_tidetabletimes( np.datetime64('2020-10-13 12:48'), method='nearest_HT' ).values == 8.01
+    check3 = tg.get_tidetabletimes( np.datetime64('2020-10-13 12:48'), method='nearest_1' ).time.values == np.datetime64('2020-10-13 14:36')
+    check4 = np.array_equal( tg.get_tidetabletimes( np.datetime64('2020-10-13 12:48'), method='nearest_2' ).values, [2.83, 8.01] )
+    check5 = np.array_equal( tg.get_tidetabletimes( np.datetime64('2020-10-13 12:48'), method='window', winsize=24 ).values,  [3.47, 7.78, 2.8 , 8.01, 2.83, 8.45, 2.08, 8.71])
+
+    if check1 and check2 and check3 and check4 and check5:
+        print(str(sec) + chr(subsec) + " OK - Tide table processing")
+    else:
+        print(str(sec) + chr(subsec) + " X - Tide table processing")    
+except:
+    print(str(sec) + chr(subsec) +' FAILED.')
+
 '''
 ###############################################################################
 ## ( 8 ) Isobath Contour Methods                                            ##
