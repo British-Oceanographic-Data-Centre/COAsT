a00171aff331cc925ccbbd3a7f1e904c3d32c768
Anthony Wise <anwise@noc.ac.uk>
2020-10-20 14:53:28 +0100

    stash

--- coast/eof.py
+++ coast/eof.py
@@ -66,6 +66,72 @@ class EOF:
         
         self.dataset['variance'] = (xr.DataArray(variance_explained, 
                 coords={'mode':(('mode'),np.arange(1,T+1))}, dims=['mode']))
+        
+def hilbert_eof(variable:xr.DataArray, full_matrices=False, time_dim_name:str='t_dim'):
+    
+        I,J,T = np.shape(variable.data)
+        F = np.nan_to_num( np.reshape(variable.data,(I*J, T)) )
+                      
+        # Remove constant zero point such as land points
+        active_ind = np.where(F.any(axis=1))[0] 
+        A = F[active_ind,:] 
+        # Remove time mean at each grid point
+        mean = A.mean(axis=1)
+        A = A - mean[:, np.newaxis]
+        
+        C = np.empty([A.shape[0],A.shape[1]],dtype=np.complex64)
+        C = signal.hilbert(A, axis=1)
+        #for i in range(A.shape[0]):
+        #    C[i,:] = signal.hilbert(A[i,:])
+        
+        P, D, Q = sp.linalg.svd(C,full_matrices=0)
+        
+        # Calculate eofs and pcs using SVD        
+        P, D, Q = linalg.svd( A, full_matrices=full_matrices )
+        EOFs = np.zeros_like(F, dtype=P.dtype)
+        # EOFs if we didn't normalise by std
+        EOFs[active_ind,:] = P 
+        #EOFs[active_ind,:] = std[:,np.newaxis] * P # if normalised by std, get back units
+        
+        # Calculate variance explained
+        if full_matrices:        
+            variance_explained = 100.*( D**2 / np.sum( D**2 ) )
+        else:
+            Inv = P.dot( np.dot( np.diag(D), Q ) ) # PDQ
+            var1 = np.sum( np.var( Inv, axis=1, ddof=1 ) )
+            var2 = np.sum( np.var( A, axis=1, ddof=1 ) )
+            mult = var1 / var2
+            variance_explained = 100.*mult*( D**2 / np.sum( D**2 ) )
+        
+        # Reshape and scale PCs
+        PCs = np.transpose(Q) * D
+        scale = np.max(np.abs(PCs),axis=0)
+        EOFs = np.reshape(EOFs, (I,J,T)) * scale
+        PCs = np.transpose(Q) * D / scale
+        
+              
+        # Assign to xarray variables
+        # copy the coordinates 
+        coords = {'mode':(('mode'),np.arange(1,T+1))}
+        time_coords = {'mode':(('mode'),np.arange(1,T+1))}
+        for coord in variable.coords:
+            if variable.dims[2] not in variable[coord].dims:
+                coords[coord] = (variable[coord].dims, variable[coord])
+            else:
+                if (variable.dims[2],) == variable[coord].dims:
+                    time_coords[coord] = (variable[coord].dims, variable[coord])
+        dataset = xr.Dataset()
+        dims = (variable.dims[:2]) + ('mode',)
+        dataset['EOF'] = xr.DataArray(EOFs, coords=coords, dims=dims)
+        dataset.EOF.attrs['standard name'] = 'EOFs'
+        if 'units' in variable.attrs:
+            dataset.EOF.attrs['units'] = variable.units
+        
+        dims = (variable.dims[2],'mode')
+        dataset['PC'] = xr.DataArray(PCs, coords=time_coords, dims=dims)
+        
+        dataset['variance'] = (xr.DataArray(variance_explained, 
+                coords={'mode':(('mode'),np.arange(1,T+1))}, dims=['mode']))
                 
         
         
