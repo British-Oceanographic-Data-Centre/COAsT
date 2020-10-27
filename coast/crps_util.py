7289c2612020835e0b7c6f8f1b0a56828a75bf1e
david <dbyrne@noc.ac.uk>
2020-10-27 13:15:23 +0000

    CRPS moved to crps_utils. subset_distance moved to general_utils

--- coast/crps_util.py
+++ coast/crps_util.py
@@ -9,8 +9,121 @@ Probability Score.
 
 import numpy as np
 import xarray as xr
-from .CDF import CDF
+from . import general_utils
 
+def crps_empirical(sample, obs):
+        """Calculates CRPS for a single observations against a sample of values.
+        This sample of values may be an ensemble of model forecasts or a model
+        neighbourhood. This is a comparison of a Heaviside function defined by
+        the observation value and an Empirical Distribution Function (EDF)
+        defined by the sample of values. This sample is sorted to create the 
+        EDF.
+        
+        The calculation method is that outlined by Hersbach et al. (2000). 
+        Each member of a supplied sample is weighted equally.
+        
+        Args:
+            sample (array): Array of points (ensemble or neighbourhood)
+            xa (float): A single 'observation' value which to compare against
+                        CDF.
+
+        Returns:
+            A single CRPS value.
+        """
+
+        def calc(alpha, beta, p):
+            return alpha * p**2 + beta*(1 - p)**2
+        
+        xa = float(obs)
+        crps_integral = 0
+        sample = np.array(sample)
+        sample = sample[~np.isnan(sample)]
+        sample = np.sort(sample)
+        sample_size = len(sample)
+        
+        alpha = np.zeros(sample_size-1)
+        beta= np.zeros(sample_size-1)
+        # sample[1:] = upper bounds, and vice versa
+        
+        tmp = sample[1:] - sample[:-1]
+        tmp_logic = sample[1:]<xa
+        alpha[tmp_logic] = tmp[tmp_logic]
+                               
+        tmp_logic = sample[:-1]>xa
+        beta[tmp_logic] = tmp[tmp_logic]
+        
+        tmp_logic = ( sample[1:]>xa )*( sample[:-1]<xa )
+        tmp = xa - sample[:-1]
+        alpha[tmp_logic] = tmp[tmp_logic]
+        tmp = sample[1:] - xa
+        beta[tmp_logic] = tmp[tmp_logic]
+        
+        p = np.arange(1,sample_size)/sample_size
+        c = alpha*p**2 + beta*(1-p)**2
+        crps_integral = np.sum(c)
+        
+        # Intervals 0 and N, where p = 0 and 1 respectively
+        if xa < sample[0]:
+            p=0
+            alpha = 0
+            beta = sample[0] - xa
+            crps = calc(alpha, beta, p)
+            crps_integral += crps
+        elif xa > sample[-1]:
+            p=1
+            alpha = xa - sample[-1]
+            beta = 0
+            crps = calc(alpha, beta, p)
+            crps_integral += crps
+            
+        crps = crps_integral
+            
+        return crps
+    
+def crps_empirical_loop(sample, obs):
+        """Like crps_empirical, however a loop is used instead of numpy 
+        boolean indexing. For large samples, will be slower but consume less
+        memory.
+        """
+        
+        def calc(alpha, beta, p):
+            return alpha * p**2 + beta*(1 - p)**2
+        crps_integral = 0
+        sample = np.array(sample)
+        sample = sample[~np.isnan(sample)]
+        sample = np.sort(sample)
+        sample_size = len(sample)
+        
+        # All intervals within range of the sample distribution
+        for ii in range(0, sample_size-1):
+            p = (ii+1)/sample_size
+            if obs > sample[ii+1]:
+                alpha = sample[ii+1] - sample[ii]
+                beta = 0
+            elif obs < sample[ii]:
+                alpha = 0
+                beta = sample[ii+1] - sample[ii]
+            else:
+                alpha = obs - sample[ii]
+                beta = sample[ii+1] - obs
+            crps = calc(alpha, beta, p)
+            crps_integral += crps
+        # Intervals 0 and N, where p = 0 and 1 respectively
+        if obs < sample[0]:
+            p=0
+            alpha = 0
+            beta = sample[0] - obs
+            crps = calc(alpha, beta, p)
+            crps_integral += crps
+        elif obs > sample[-1]:
+            p=1
+            alpha = obs - sample[-1]
+            beta = 0
+            crps = calc(alpha, beta, p)
+            crps_integral += crps
+            
+        return crps_integral
+    
 def crps_sonf_fixed( mod_array, obs_lon, obs_lat, obs_var, obs_time, 
                       nh_radius: float, cdf_type:str, time_interp:str
     ):
@@ -51,7 +164,7 @@ def crps_sonf_fixed( mod_array, obs_lon, obs_lat, obs_var, obs_time,
     neighbourhood_indices = np.arange(0,n_neighbourhoods)
     
     # Get model neighbourhood subset using specified method
-    subset_ind = subset_indices_by_distance(
+    subset_ind = general_utils.subset_indices_by_distance(
                      mod_array.longitude, mod_array.latitude, 
                      obs_lon, obs_lat, nh_radius)
     mod_subset = mod_array.isel(y_dim = subset_ind[0],
@@ -77,10 +190,11 @@ def crps_sonf_fixed( mod_array, obs_lon, obs_lat, obs_var, obs_time,
                     pass
                 else:
                     # Create model and observation CDF objects
-                    mod_cdf = CDF(mod_subset_time, cdf_type = cdf_type)
+                    #mod_cdf = CDF(mod_subset_time, cdf_type = cdf_type)
                 
                     # Calculate CRPS and put into output array
-                    crps_list[ii] = mod_cdf.crps_fast(obs_var[ii])
+                    crps_list[ii] = crps_empirical(mod_subset_time, 
+                                                   obs_var[ii])
                     n_model_pts[ii] = int(mod_subset.shape[0])
 
     return crps_list, n_model_pts, contains_land
@@ -129,7 +243,7 @@ def crps_sonf_moving( mod_array, obs_lon, obs_lat, obs_var, obs_time,
         cntr_lat = obs_lat[ii]
     
         # Get model neighbourhood subset using specified method
-        subset_ind = subset_indices_by_distance(
+        subset_ind = general_utils.subset_indices_by_distance(
                          mod_array.longitude, mod_array.latitude, 
                          cntr_lon, cntr_lat, nh_radius)
         
@@ -153,10 +267,10 @@ def crps_sonf_moving( mod_array, obs_lon, obs_lat, obs_var, obs_time,
                 pass
             else:
                 # Create model and observation CDF objects
-                mod_cdf = CDF(mod_subset, cdf_type = cdf_type)
+                # mod_cdf = CDF(mod_subset, cdf_type = cdf_type)
             
                 # Calculate CRPS and put into output array
-                crps_list[ii] = mod_cdf.crps_fast(obs_var[ii])
+                crps_list[ii] = crps_empirical(mod_subset, obs_var[ii])
                 n_model_pts[ii] = int(mod_subset.shape[0])
                 
     return crps_list, n_model_pts, contains_land
