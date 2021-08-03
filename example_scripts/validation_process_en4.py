aadc1804c6ef8f735a08c4d6bcaf45e293202a0a
davbyr <dbyrne@noc.ac.uk>
2021-08-03 14:03:33 +0100

    shebang added to process script

--- example_scripts/validation_process_en4.py
+++ example_scripts/validation_process_en4.py
@@ -5,6 +5,7 @@ EN4 data. The processing will extract a domain of the data and apply QC flags.
 The processed data will be saved to a new file, ready for use in the analysis
 routines.
 """
+#!/usr/bin/env python3
 
 import sys
 # IF USING A DEVELOPMENT BRANCH OF COAST, ADD THE REPOSITORY TO PATH:
@@ -17,7 +18,7 @@ import numpy as np
 #    fn_en4_in can be multiple files by using a glob (*).
 #    fn_en4_out will be a single (concatenated) output file
 fn_en4_in = "/gws/nopw/j04/jmmp/CO9_AMM15/obs/en4/*.nc"
-fn_en4_out = "/gws/nopw/j04/jmmp/CO9_AMM15/obs/en4_processed_amm15.nc"
+fn_en4_out = "/gws/nopw/j04/jmmp/CO9_AMM15/obs/en4_processed2_amm15.nc"
 
 # 2) Define longitude and latitude box for which to extract EN4 data.
 #    This should match your model domain at least approximately.
@@ -28,7 +29,7 @@ latbounds = [44.06, 63.35]
 
 # 3) Create COAsT PROFILE() object and read in EN4 data to its dataset
 en4 = coast.PROFILE()
-en4.read_EN4(fn_en4_in, multiple=True, chunks={'profile':10000})
+en4.read_EN4(fn_en4_in, multiple=True, chunks={'N_PROF':10000})
 
 # 4) Call the processing routine, which will automaticall write to file
 en4.process_en4(fn_en4_out, lonbounds, latbounds)
