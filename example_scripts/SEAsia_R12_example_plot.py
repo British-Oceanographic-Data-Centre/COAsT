ef08ec080cd538b16f33aac639d6924d803111fd
jpolton <jelt@noc.ac.uk>
2021-09-10 17:26:09 +0100

    Gridded() update for easy examples

--- example_scripts/SEAsia_R12_example_plot.py
+++ example_scripts/SEAsia_R12_example_plot.py
@@ -20,15 +20,15 @@ import matplotlib.colors as colors # colormap fiddling
 dir_nam = "/projectsa/COAsT/NEMO_example_data/SEAsia_R12/"
 fil_nam = "SEAsia_R12_5d_20120101_20121231_gridT.nc"
 dom_nam = "domain_cfg_ORCA12_adj.nc"
+config_t = "/work/jelt/GitHub/COAsT/example_files/example_t_nemo_config.json"
 
-
-sci_t = coast.NEMO(dir_nam + fil_nam, \
-        dir_nam + dom_nam, grid_ref='t-grid', multiple=False)
-
+sci_t = coast.Gridded(dir_nam + fil_nam, \
+        dir_nam + dom_nam, config=config_t)
+    
 #%% Plot
 fig = plt.figure()
 
-plt.pcolormesh( sci_t.dataset.longitude, sci_t.dataset.latitude, sci_t.dataset.soce.isel(t_dim=0).isel(z_dim=0))
+plt.pcolormesh( sci_t.dataset.longitude, sci_t.dataset.latitude, sci_t.dataset.salinity.isel(t_dim=0).isel(z_dim=0))
 plt.xlabel('longitude')
 plt.ylabel('latitude')
 plt.title('SE Asia, surface salinity (psu)')
