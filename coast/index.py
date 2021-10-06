e65e22583b2390c945f685d7ed0799bf0cb9bd86
BlackBot <bodcsoft@bodc.ac.uk>
2021-10-06 16:32:32 +0000

    Apply Black formatting to Python code.

--- coast/index.py
+++ coast/index.py
@@ -8,6 +8,7 @@ from ast import literal_eval
 from .coast import Coast
 from .config_parser import ConfigParser
 
+
 def setup_dask_client(workers: int = 2, threads: int = 2, memory_limit_per_worker: str = "2GB"):
     Client(n_workers=workers, threads_per_worker=threads, memory_limit=memory_limit_per_worker)
 
