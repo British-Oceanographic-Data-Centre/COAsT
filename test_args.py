e0939abebdd759255a08ac5e915cda72446742e1
Rob Jennings <roje@bodc.ac.uk>
2022-06-20 11:53:05 +0100

    Deleted test file

--- test_args.py
+++
@@ -1,9 +0,0 @@
-class Test:
-    def test_method(self, *, arg1: str, arg2: str):
-        print(arg1)
-        print(arg2)
-
-
-t1 = Test()
-t1.test_method("test1", "test2") # This fails due to unexpected number of arguments.
-t1.test_method(arg1 = "test1", arg2 = "test2") # It passes when using named arguments.
