908b29823ef1f623fae9e258e67b45938368382d
lukeg <lukgor@noc.ac.uk>
2022-06-27 14:51:48 +0100

    switch on IS_ACTION

--- unit_testing/unit_test_files.py
+++ unit_testing/unit_test_files.py
@@ -9,7 +9,8 @@ For example:
     get_dom = unit_test_files.fn_nemo_dom
 """
 
-import os.path as path
+from os import path, getenv, system
+import sys
 
 # All directories relative to unit_testing diretory
 dn_files = "./example_files/"  # Example data directory
@@ -17,8 +18,11 @@ dn_config = "./config"  # Example config file directory
 dn_fig = "./unit_testing/figures/"  # Figure saving directory
 dn_scripts = "./example_scripts"  # Example scripts directory
 
+# skip if it's an action and the files don't exist
+# want it to run if not action and files don't exist
+
 # Check dn_files directory
-if not path.isdir(dn_files):
+if getenv("IS_ACTION") == None and not path.isdir(dn_files):
     print("please go download the examples file from https://linkedsystems.uk/erddap/files/COAsT_example_files/")
     dn_files = input("what is the path to the example files:\n")
     if not path.isdir(dn_files):
