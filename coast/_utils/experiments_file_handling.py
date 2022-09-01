fdcc7fc297a48183fa46743796ae62e8af9a4323
thogar <thogar@noc.ac.uk>
2022-09-01 15:28:09 +0100

    code review change
    This change is simplify the nemo file name method and added in typing

--- coast/_utils/experiments_file_handling.py
+++ coast/_utils/experiments_file_handling.py
@@ -1,6 +1,7 @@
-"""Set of functions to control basic experimnet file handling"""
-import numpy as np
+"""Set of functions to control basic experiment file handling"""
 
+import json
+from typing import List
 
 def experiments(experiments="experiments.json"):
     """
@@ -22,8 +23,6 @@ def experiments(experiments="experiments.json"):
     exp_names,dirs,domains,file_names
 
     """
-    import json
-
     with open(experiments, "r") as j:
         json_content = json.loads(j.read())
         try:
@@ -43,42 +42,32 @@ def experiments(experiments="experiments.json"):
         except:
             file_names = []
 
-        # print(lengths)
         # check all non zero lengths are the same
         lengths = np.array([len(exp_names), len(dirs), len(domains), len(file_names)])
         if np.min(lengths[np.nonzero(lengths)[0]]) != np.max(lengths[np.nonzero(lengths)[0]]):
             print("Warning DIFFERENT NUMBER OF NAMES PROVIDED, CHECK JSON FILE")
     return exp_names, dirs, domains, file_names
 
+def nemo_filename_maker(directory, year_start: int, year_stop: int, grid: str = "T") -> List:
+    """Creates a list of NEMO file names from a set of standard templates.
 
-def nemo_filenames(dpath, runtype, ystart, ystop, grid="T"):
+    Args:
+        directory: path to the files'
+        year_start: start year
+        year_stop: stop year
+        grid: NEMO grid type defaults to T
+
+    Returns: a list of possible nemo file names
+
     """
-    Creates a list of NEMO file names from a set of standard templates
-     Parameters
-     ----------
-     dpath : path to the files
-     runtype : hardwired set of standard nemo filenames
-     ystart : start year
-     ystop : stop year
-     grid (optional) NEMO grid type
-
-     -------
-     names :lis tof nemo file names
-    """
-
     # produce a list of nemo filenames
-    names = np.array([])
-    if runtype == "SENEMO":
-        for iy in range(ystart, ystop + 1):
-            for im in range(1, 12 + 1):
-                MNTH = str(im)
-                if im < 10:
-                    MNTH = "0" + MNTH
-                YEAR = str(iy)
-                new_name = "{0}/SENEMO_1m_{1}0101_{1}1231_grid_{3}_{1}{2}-{1}{2}.nc".format(dpath, YEAR, MNTH, grid)
-                names = np.append(names, new_name)
+    names = []
+    january = 1
+    december = 13  # range is non-inclusive so we need 12 + 1
 
-    else:
-        print("Runtype: " + runtype + "not coded yet, returning empty list")
-        names = []
+    for year in range(year_start, year_stop + 1):
+        for month in range(january, december):
+            new_name = f"{directory}/SENEMO_1m_{year}0101_{year}1231_grid_{grid}_{year}{month:02}-{year}{month:02}.nc"
+            names.append(new_name)
+
     return names
