0bb6351052b2c50cbee5b4602d858a738350043d
lukeg <lukgor@noc.ac.uk>
2022-09-05 14:12:32 +0100

    specify exception type

--- coast/diagnostics/contour.py
+++ coast/diagnostics/contour.py
@@ -362,19 +362,19 @@ class ContourF(Contour):
         tmp_velocities = xr.full_like(u_ds.u_velocity, np.nan)
         try:
             tmp_velocities[:, :, dr_n] = u_ds.u_velocity.data[:, :, dr_n + 1]
-        except:
+        except IndexError:
             print("no northward sections")
         try:
             tmp_velocities[:, :, dr_s] = -u_ds.u_velocity.data[:, :, dr_s]
-        except:
+        except IndexError:
             print("no southward sections")
         try:
             tmp_velocities[:, :, dr_e] = -v_ds.v_velocity.data[:, :, dr_e + 1]
-        except:
+        except IndexError:
             print("no eastward sections")
         try:
             tmp_velocities[:, :, dr_w] = v_ds.v_velocity.data[:, :, dr_w]
-        except:
+        except IndexError:
             print("no westward sections")
         self.data_cross_flow["normal_velocities"] = tmp_velocities[:, :, :-1]
         self.data_cross_flow["normal_velocities"].attrs = {"units": "m/s", "standard_name": "contour-normal velocities"}
