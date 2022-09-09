a2672c0b3b12c7d324a66da6d98ed7af8448ec6f
jpolton <jpolton@gmail.com>
2022-09-09 10:13:05 +0100

    Update __init__.py

--- coast/__init__.py
+++ coast/__init__.py
@@ -8,7 +8,6 @@ from .diagnostics.gridded_stratification import GriddedStratification
 from .diagnostics.climatology import Climatology
 from ._utils import logging_util, general_utils, plot_util, crps_util, seasons
 from .diagnostics.annual_hydrographic_climatology import Annual_Climatology
-from ._utils import logging_util, general_utils, plot_util, crps_util
 from .data.index import Indexed
 from .data.profile import Profile
 from .diagnostics.profile_analysis import ProfileAnalysis
