35a6f8b73d06a4e3e7d2a42c674abe2e679c563a
lukeg <lukgor@noc.ac.uk>
2022-09-13 16:38:19 +0100

    fix path

--- notebook_to_md.sh
+++ notebook_to_md.sh
@@ -37,7 +37,7 @@ EOM
     # Echo hugo header to beginning of generated md file.
     echo "$VAR" | cat - $FILE > temp && mv temp $FILE
     sed -i "s+${filename}_files/+/COAsT/${filename}_files/+g" $FILE
-    [ -d /example_scripts/notebooks/markdown/${directory}/${filename}_files ] && mv  example_scripts/notebooks/markdown/${directory}/${filename}_files example_scripts/notebooks/markdown_images/${directory}/
+    [ -d example_scripts/notebooks/markdown/${directory}/${filename}_files ] && mv  example_scripts/notebooks/markdown/${directory}/${filename}_files example_scripts/notebooks/markdown_images/${directory}/
     echo "script is all done"
 done
 
