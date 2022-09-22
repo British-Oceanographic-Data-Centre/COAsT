3f076b89027d38c85adfcda2a925e5045c0accde
jp <jpolton@gmail.com>
2022-09-22 10:29:47 +0100

    update package version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from types import SimpleNamespace
 PACKAGE = SimpleNamespace(
     **{
         "name": "COAsT",
-        "version": "3.1.0",
+        "version": "3.1.1",
         "description": "This is the Coast Ocean Assessment Tool",
         "url": "https://www.bodc.ac.uk",
         "download_url": "https://github.com/British-Oceanographic-Data-Centre/COAsT/",
