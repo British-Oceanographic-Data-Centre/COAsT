ea5bc5d312e94094dd28752687af191ae69354bf
Lee de Mora <ledm@pml.ac.uk>
2022-10-12 13:37:26 +0100

    fixed dangling file openings

--- coast/data/coast.py
+++ coast/data/coast.py
@@ -85,7 +85,8 @@ class Coast:
             chunks (Dict): Chunks to use in Dask [default None].
         """
         info(f"Loading a single file ({file} for {get_slug(self)}")
-        self.dataset = xr.open_dataset(file, chunks=chunks)
+        with xr.open_dataset(file, chunks=chunks) as xrfile:
+            self.dataset = xrfile
 
     def load_multiple(self, directory_to_files: str, chunks: Dict = None):
         """Loads multiple files from directory into dataset variable.
@@ -95,7 +96,11 @@ class Coast:
             chunks (Dict): Chunks to use in Dask [default None].
         """
         info(f"Loading a directory ({directory_to_files}) for {get_slug(self)}")
-        self.dataset = xr.open_mfdataset(directory_to_files, chunks=chunks, parallel=True, combine="by_coords")
+        with xr.open_mfdataset(directory_to_files, 
+            chunks=chunks, 
+            parallel=True, 
+            combine="by_coords") as files:
+                self.dataset = files
 
     def load_dataset(self, dataset: xr.Dataset):
         """Loads a dataset.
