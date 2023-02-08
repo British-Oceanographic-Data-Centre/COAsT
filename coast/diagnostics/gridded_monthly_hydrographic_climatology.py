78be971f9674ea1cad1235691835d85c4558b160
Jason Holt <jholt@sci5.jasmin.ac.uk>
2023-02-08 17:11:15 +0000

    update gridded_monthly_hydrographic_climatology.py

--- coast/diagnostics/gridded_monthly_hydrographic_climatology.py
+++ coast/diagnostics/gridded_monthly_hydrographic_climatology.py
@@ -44,7 +44,7 @@ class GriddedMonthlyHydrographicClimatology(Gridded):
                 print(itt)
                 gridded_t2 = gridded_t.subset_as_copy(t_dim=itt)
                 print("copied", im)
-                PEA = GriddedStratification(gridded_t2, gridded_t2)
+                PEA = GriddedStratification(gridded_t2)
                 PEA.calc_pea(gridded_t2, Zd_mask)
                 PEA_monthy_clim[im, :, :] = PEA_monthy_clim[im, :, :] + PEA.dataset["PEA"].values
         PEA_monthy_clim = PEA_monthy_clim / nyear
