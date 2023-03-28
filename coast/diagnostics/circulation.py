c5d6252b4372acd95f917b1131ed33c3051072d2
jpolton <jpolton@gmail.com>
2023-03-28 18:30:15 +0100

    Remove index method (hopefully more scalable...)

--- coast/diagnostics/circulation.py
+++ coast/diagnostics/circulation.py
@@ -15,7 +15,7 @@ class CurrentsOnT(Gridded):
     """
     Methods for currents co-located on T-points
 
-    use like coast.Gridded () with a domain_cfg file name to create gridded object
+    Use like coast.Gridded() with a domain_cfg file name to create gridded object
 
     """
 
@@ -25,7 +25,20 @@ class CurrentsOnT(Gridded):
 
     def currents_on_t(self, gridded_u, gridded_v):
         """
-        Adds co-located velocity components and speed to CurrentsonT object
+        Adds co-located velocity components and speed to CurrentsOnT object
+
+        Avoids use of indices in the hope that it will be more parallelisable. However in index space the mappings
+        take this form:
+        u_on_t_points[:, :, :, 1:] = 0.5 * (
+            gridded_u.dataset.u_velocity.values[:, :, :, 1:] + gridded_u.dataset.u_velocity.values[:, :, :, :-1]
+            )
+
+        v_on_t_points[:, :, 1:, :] = 0.5 * (
+            gridded_v.dataset.v_velocity.values[:, :, 1:, :] + gridded_v.dataset.v_velocity.values[:, :, :-1, :]
+            )
+
+        For arrays of shape and order: (z_dim, t_dim, y_dim, x_dim)
+
         Parameters
         ----------
         gridded_u : TYPE
@@ -38,24 +51,14 @@ class CurrentsOnT(Gridded):
         None.
 
         """
-        print(self.grid_ref)
-        #self.dataset["vt_velocity"] = 0.5*(gridded_v.dataset.v_velocity.shift(y_dim=1) + gridded_v.dataset.v_velocity)
-        ds_u = self.dataset.copy(deep=True) 
-        # U velocity on T points
-        #UT = np.zeros(gridded_u.dataset.u_velocity.shape)
-        #self.dataset["ut_velocity"] = xr.where(  # zero at long min, else average across t-point
-        #        gridded_u.dataset.longitude == gridded_u.dataset.longitude.isel(x_dim=0), 0, 
-        #                0.5*(gridded_u.dataset.u_velocity.shift(x_dim=+1) + 
-        #                     gridded_u.dataset.u_velocity)
-        #        )
-        
+        ds_u = self.dataset.copy(deep=True)
+        # U velocity on T-points
         ds_u["ut_velocity"] = 0.5*(gridded_u.dataset.u_velocity.shift(x_dim=1) + gridded_u.dataset.u_velocity)
         # replace wrapped (1st) longitude coord with zero
         _, _lon = xr.broadcast(gridded_u.dataset.u_velocity, gridded_u.dataset.longitude)
-        ds_u["ut_velocity"] = ds_u["ut_velocity"].where( _lon == _lon.isel(x_dim=0),  0)
+        ds_u["ut_velocity"] = ds_u["ut_velocity"].where(_lon != _lon.isel(x_dim=0), 0) # keep values except where lon(x_dim=0)
         del _, _lon
-
-        ds_u.coords['latitude']  = self.dataset.latitude
+        ds_u.coords['latitude'] = self.dataset.latitude
         ds_u.coords['longitude'] = self.dataset.longitude
         ds_u.coords['depth_0'] = self.dataset.depth_0
         try:
@@ -63,28 +66,14 @@ class CurrentsOnT(Gridded):
         except:
             self.dataset["ut_velocity"] = ds_u.vt_velocity 
             debug('Did not find depthu variable to drop - to avoid conflicts in z_dim dimension')
-       
 
-        
-        #UT[:, :, :, 1:] = 0.5 * (
-        #    gridded_u.dataset.u_velocity.values[:, :, :, 1:] + gridded_u.dataset.u_velocity.values[:, :, :, :-1]
-        #)
-        # V velocity on T points
+        # V velocity on T-points
         ds_v = self.dataset.copy(deep=True) 
         
         ds_v["vt_velocity"] = 0.5*(gridded_v.dataset.v_velocity.shift(y_dim=1) + gridded_v.dataset.v_velocity)
         # replace wrapped (1st) latitude coord with zero
         _, _lat = xr.broadcast(gridded_v.dataset.v_velocity, gridded_v.dataset.latitude)
-        ds_v["vt_velocity"] = ds_v["vt_velocity"].where( _lat == _lat.isel(y_dim=0), 0)
-        #self.dataset["vt_velocity"] = xr.where(  #zero at lat min, else average across t-point
-        #        gridded_v.dataset.latitude == gridded_v.dataset.latitude.isel(y_dim=0), 0, 
-        #                0.5*(gridded_v.dataset.v_velocity.shift(y_dim=+1) + 
-        #                     gridded_v.dataset.v_velocity)
-        #        )
-        #VT = np.zeros(gridded_v.dataset.v_velocity.shape)
-        #VT[:, :, 1:, :] = 0.5 * (
-        #    gridded_v.dataset.v_velocity.values[:, :, 1:, :] + gridded_v.dataset.v_velocity.values[:, :, :-1, :]
-        #)
+        ds_v["vt_velocity"] = ds_v["vt_velocity"].where(_lat != _lat.isel(y_dim=0), 0) # keep values except where lat(y_dim=0)
         del _, _lat
         ds_v.coords['latitude']  = self.dataset.latitude
         ds_v.coords['longitude'] = self.dataset.longitude
@@ -94,11 +83,10 @@ class CurrentsOnT(Gridded):
         except:
             self.dataset["vt_velocity"] = ds_v.vt_velocity 
             debug('Did not find depthv variable to drop - to avoid conflicts in z_dim dimension')
-        #speed = np.sqrt(UT * UT + VT * VT)
-        self.dataset["speed_t"] = np.sqrt( np.square(self.dataset["ut_velocity"]) + np.square(self.dataset["vt_velocity"]) )       #dims = gridded_u.dataset.u_velocity.dims
-        #self.dataset["ut_velocity"] = xr.DataArray(UT, dims=dims)
-        #self.dataset["vt_velocity"] = xr.DataArray(VT, dims=dims)
-        #self.dataset["speed_t"] = xr.DataArray(speed, dims=dims)
+
+        # Speed on T-points
+        self.dataset["speed_t"] = np.sqrt( np.square(self.dataset["ut_velocity"]) + np.square(self.dataset["vt_velocity"]))
+
 
     def quick_plot(self, name, Vmax=0.16, Np=3, headwidth=4, scale=50, **kwargs):
         """
