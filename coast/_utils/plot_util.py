a443e8eace10b9555072476f286f30bf89dc7bc8
Ben Barton <benbar@noc.ac.uk>
2023-10-31 16:42:23 +0000

    Need to test transform(lon, lat)

--- coast/_utils/plot_util.py
+++ coast/_utils/plot_util.py
@@ -437,8 +437,9 @@ def grid_angle(lon, lat):
         for i in range(lon.shape[1] - 1):
             crs_aeqd = make_projection(lon[j, i], lat[j, i])
             to_metre = pyproj.Transformer.from_crs(crs_wgs84, crs_aeqd)
-            x_grid, y_grid = to_metre.transform(lat[j:j + 2, i:i + 2], 
-                                                lon[j:j + 2, i:i + 2])
+            # not sure if this should be lat, lon or lon, lat
+            (x_grid, y_grid) = to_metre.transform(lon[j:j + 2, i:i + 2], 
+                                                lat[j:j + 2, i:i + 2])
             angle[j, i] = np.arctan2((x_grid[1, 0] - x_grid[0, 0]), 
                 (y_grid[1, 0] - y_grid[0, 0])) * (180 / np.pi) # relative to North
     
