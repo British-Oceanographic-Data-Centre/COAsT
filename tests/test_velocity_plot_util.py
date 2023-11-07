b34bce55e56e7cf9134c2099e485145b8d083b87
BlackBot <bodcsoft@bodc.ac.uk>
2023-11-07 18:09:08 +0000

    Apply Black formatting to Python code.

--- tests/test_velocity_plot_util.py
+++ tests/test_velocity_plot_util.py
@@ -1,10 +1,11 @@
 # IMPORT modules. Must have pytest.
 
-#import os.path as path
-#import pytest
+# import os.path as path
+# import pytest
 import numpy as np
 import pyproj
-#import sys
+
+# import sys
 from coast._utils import plot_util
 
 # IMPORT THIS TO HAVE ACCESS TO EXAMPLE FILE PATHS:
@@ -76,8 +77,7 @@ def test_velocity_grid_to_geo():
     u_velocity = np.array(([1, 1, 1], [1, 1, 1], [1, 1, 1]))
     v_velocity = np.array(([1, 1, 1], [1, 1, 1], [1, 1, 1]))
 
-    u_new, v_new = plot_util.velocity_grid_to_geo(lon, lat, u_velocity, v_velocity,
-                                                  polar_stereo=False)
+    u_new, v_new = plot_util.velocity_grid_to_geo(lon, lat, u_velocity, v_velocity, polar_stereo=False)
     u_result1 = np.array(
         (
             [1.04903051, 1.05046004, 1.05188719],
@@ -94,15 +94,10 @@ def test_velocity_grid_to_geo():
     )
     assert np.isclose(u_new, u_result1).all() & np.isclose(v_new, v_result1).all()
 
-    u_new, v_new = plot_util.velocity_grid_to_geo(lon, lat, u_velocity, v_velocity,
-                                                  polar_stereo=True)
+    u_new, v_new = plot_util.velocity_grid_to_geo(lon, lat, u_velocity, v_velocity, polar_stereo=True)
     print(u_new, v_new)
     u_result2 = np.array(
-        (
-            [1.222728, 1.21099989, 1.19965573],
-            [1.28512188, 1.27230937, 1.25958666],
-            [1.34721927, 1.33542729, 1.32321739]
-        )
+        ([1.222728, 1.21099989, 1.19965573], [1.28512188, 1.27230937, 1.25958666], [1.34721927, 1.33542729, 1.32321739])
     )
     v_result2 = np.array(
         (
