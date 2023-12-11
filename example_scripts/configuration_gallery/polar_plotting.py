ee49861407e3ca44d98a64708d1c176fb3b8f187
BlackBot <bodcsoft@bodc.ac.uk>
2023-12-11 12:29:54 +0000

    Apply Black formatting to Python code.

--- example_scripts/configuration_gallery/polar_plotting.py
+++ example_scripts/configuration_gallery/polar_plotting.py
@@ -5,14 +5,14 @@ Polar plotting example
 #################################################
 # This demonstration will show how to re-project the NEMO velocities for quiver
 # plotting in polar coordinates.
-# 
+#
 # NEMO velocities are usually calculated and saved in along grid i and j directions.
 # This causes an issue when plotting velocities as vectors on a map where it is
 # assumed that i and j velocities point eastwards and northwards but the grid is
 # curvilinear.
-# 
+#
 # There are additional isses when plotting quivers over the poles that we will cover.
-# 
+#
 # The data we use here comes from a global model that has been cropped to the Arctic region.
 #################################################
 # %%
@@ -40,21 +40,24 @@ fn_nemo_config_v = root + "config/gc31_nemo_grid_v.json"
 fn_nemo_dom = dn_files + "coast_nemo_quiver_dom.nc"
 
 # Define output filepath (optional: None or str)
-fn_out = './quiver_plot.png'
+fn_out = "./quiver_plot.png"
 
 # Read in multiyear data (This example uses NEMO data from a single file.)
-nemo_data_t = coast.Gridded(fn_data=fn_nemo_dat_t,
-                          fn_domain=fn_nemo_dom,
-                          config=fn_nemo_config_t,
-                          ).dataset
-nemo_data_u = coast.Gridded(fn_data=fn_nemo_dat_u,
-                          fn_domain=fn_nemo_dom,
-                          config=fn_nemo_config_u,
-                          ).dataset
-nemo_data_v = coast.Gridded(fn_data=fn_nemo_dat_v,
-                          fn_domain=fn_nemo_dom,
-                          config=fn_nemo_config_v,
-                          ).dataset
+nemo_data_t = coast.Gridded(
+    fn_data=fn_nemo_dat_t,
+    fn_domain=fn_nemo_dom,
+    config=fn_nemo_config_t,
+).dataset
+nemo_data_u = coast.Gridded(
+    fn_data=fn_nemo_dat_u,
+    fn_domain=fn_nemo_dom,
+    config=fn_nemo_config_u,
+).dataset
+nemo_data_v = coast.Gridded(
+    fn_data=fn_nemo_dat_v,
+    fn_domain=fn_nemo_dom,
+    config=fn_nemo_config_v,
+).dataset
 
 # Select surface u and v as an example:
 data_u = nemo_data_u[["u_velocity"]]
@@ -68,9 +71,8 @@ data_v = data_v.isel(t_dim=0, z_dim=0)
 # This function may take a while
 uv_velocities = [data_u.to_array().values[0, :, :], data_v.to_array().values[0, :, :]]
 u_new, v_new = coast._utils.plot_util.velocity_grid_to_geo(
-    nemo_data_t.longitude.values, nemo_data_t.latitude.values, 
-    uv_velocities,
-    polar_stereo_cartopy_bug_fix=False)
+    nemo_data_t.longitude.values, nemo_data_t.latitude.values, uv_velocities, polar_stereo_cartopy_bug_fix=False
+)
 
 # Apply the CartoPy stereographic polar correction
 # NOTE: This could have been applied automatically with `polar_stereo=True` in
@@ -85,16 +87,16 @@ mrc = ccrs.NorthPolarStereo(central_longitude=0.0)
 
 #################################################
 # Below shows the u and v velocities when plotted with and without adjustment.
-# The plot shows three cases: 1:no correction, 2:the NEMO grid correction, and 3: 
+# The plot shows three cases: 1:no correction, 2:the NEMO grid correction, and 3:
 # the NEMO grid correction with polar stereographic plot correction. We also plot
 # the final corrected u and v velocities as streamlines.
-# 
+#
 # In case 1, the lower latitude velocities aren't too bad but become irregular
 # further north as the grid lines deviate form lat and lon.
-# 
+#
 # In case 2, the irregularity still persists even with the grid correction. This
 # is the result of a CartoPy bug which also worsens at high latitudes.
-# 
+#
 # In case 3, both corrections have been applied and the velocities quivers now align
 # with the route of strong current speed as would be expected.
 #################################################
@@ -122,35 +124,67 @@ for rr in range(n_r * n_c):
     coast._utils.plot_util.set_circle(ax[rr])
 
 
-cs = ax[0].pcolormesh(nemo_data_t.longitude.values, nemo_data_t.latitude.values, speed, transform=data_crs, vmin=0, vmax=0.3)
-ax[0].quiver(nemo_data_t.longitude.values, nemo_data_t.latitude.values, 
-    data_u.to_array().values[0, :, :], data_v.to_array().values[0, :, :], 
-    color='w', transform=data_crs, angles='xy', regrid_shape=40)
+cs = ax[0].pcolormesh(
+    nemo_data_t.longitude.values, nemo_data_t.latitude.values, speed, transform=data_crs, vmin=0, vmax=0.3
+)
+ax[0].quiver(
+    nemo_data_t.longitude.values,
+    nemo_data_t.latitude.values,
+    data_u.to_array().values[0, :, :],
+    data_v.to_array().values[0, :, :],
+    color="w",
+    transform=data_crs,
+    angles="xy",
+    regrid_shape=40,
+)
 
 ax[1].pcolormesh(nemo_data_t.longitude.values, nemo_data_t.latitude.values, speed, transform=data_crs, vmin=0, vmax=0.3)
-ax[1].quiver(nemo_data_t.longitude.values, nemo_data_t.latitude.values, 
-    u_new, v_new, 
-    color='w', transform=data_crs, angles='xy', regrid_shape=40)
+ax[1].quiver(
+    nemo_data_t.longitude.values,
+    nemo_data_t.latitude.values,
+    u_new,
+    v_new,
+    color="w",
+    transform=data_crs,
+    angles="xy",
+    regrid_shape=40,
+)
 
 ax[2].pcolormesh(nemo_data_t.longitude.values, nemo_data_t.latitude.values, speed, transform=data_crs, vmin=0, vmax=0.3)
-ax[2].quiver(nemo_data_t.longitude.values, nemo_data_t.latitude.values, 
-    u_pol, v_pol, 
-    color='w', transform=data_crs, angles='xy', regrid_shape=40)
+ax[2].quiver(
+    nemo_data_t.longitude.values,
+    nemo_data_t.latitude.values,
+    u_pol,
+    v_pol,
+    color="w",
+    transform=data_crs,
+    angles="xy",
+    regrid_shape=40,
+)
 
 ax[3].pcolormesh(nemo_data_t.longitude.values, nemo_data_t.latitude.values, speed, transform=data_crs, vmin=0, vmax=0.3)
-ax[3].streamplot(nemo_data_t.longitude.values, nemo_data_t.latitude.values, 
-    u_pol, v_pol, transform=data_crs, linewidth=1, density=2, color='w', zorder=101)
+ax[3].streamplot(
+    nemo_data_t.longitude.values,
+    nemo_data_t.latitude.values,
+    u_pol,
+    v_pol,
+    transform=data_crs,
+    linewidth=1,
+    density=2,
+    color="w",
+    zorder=101,
+)
 
-ax[0].set_title('1: No Correction')
-ax[1].set_title('2: NEMO Grid Correction')
-ax[2].set_title('3: Grid Correction and Polar Correction')
-ax[3].set_title('As left but with streamlines')
+ax[0].set_title("1: No Correction")
+ax[1].set_title("2: NEMO Grid Correction")
+ax[2].set_title("3: Grid Correction and Polar Correction")
+ax[3].set_title("As left but with streamlines")
 
-fig.colorbar(cs, cax=cax, orientation='horizontal')
-cax.set_xlabel('U (m s$^{-1}$)')
+fig.colorbar(cs, cax=cax, orientation="horizontal")
+cax.set_xlabel("U (m s$^{-1}$)")
 
-#fig.tight_layout(w_pad=subplot_padding, h_pad=subplot_padding)
-#fig.subplots_adjust(left=(fig_pad[0]), bottom=(fig_pad[1]), right=(1 - fig_pad[2]), top=(1 - fig_pad[3]))
+# fig.tight_layout(w_pad=subplot_padding, h_pad=subplot_padding)
+# fig.subplots_adjust(left=(fig_pad[0]), bottom=(fig_pad[1]), right=(1 - fig_pad[2]), top=(1 - fig_pad[3]))
 
 plt.show()
 # uncomment this line to save an output image
