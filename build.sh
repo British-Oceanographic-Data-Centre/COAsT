5c0428501525d0f1f866bd12052673dd886dfb5f
Magda Juarez <magjua@noc.ac.uk>
2025-06-24 18:17:51 +0100

    Update build.sh and generate_conda_meta.sh
    Update scripts to initialise conda in shell session

--- build.sh
+++ build.sh
@@ -7,6 +7,8 @@ mkdir -p build dist
 echo "Cleaning up old versions..."
 rm -rf ./build/* ./dist/* ./Example_Python_Package.egg-info
 echo "Building package for distribution..."
+CONDA_BASE=$(conda info --base)
+source "${CONDA_BASE}/etc/profile.d/conda.sh"
 conda activate coast
 python ./setup.py sdist bdist_wheel
 
