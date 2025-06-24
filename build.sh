1492943a2d6241504a93c59dcaf26d81255ca1e6
tobias ferreira <soutobias@yahoo.com.br>
2025-06-24 11:58:28 +0100

    update build package workflow

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ cd "$(dirname "$0")"  # Set working directory
 
 mkdir -p build dist
 echo "Cleaning up old versions..."
+conda activate coast
 ./venv/bin/pip install "cython<3.0.0"
 ./venv/bin/pip install --no-build-isolation PyYAML==6.0
 rm -rf ./build/* ./dist/* ./Example_Python_Package.egg-info
