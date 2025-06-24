6fbeb41aedbfd19c5c18610a991a765ba18cb424
Magda Juarez <magjua@noc.ac.uk>
2025-06-24 12:08:04 +0100

    Update build.sh
    Move conda commands to be run before installing the rest of dependencies

--- build.sh
+++ build.sh
@@ -7,11 +7,11 @@ mkdir -p build dist
 echo "Cleaning up old versions..."
 rm -rf ./build/* ./dist/* ./Example_Python_Package.egg-info
 echo "Building package for distribution..."
-./venv/bin/python ./setup.py sdist bdist_wheel
 CONDA_BASE=$(conda info --base)
 source "${CONDA_BASE}/etc/profile.d/conda.sh"
 conda create -n coast-toolbox python=3.10
 conda activate coast-toolbox
 conda install conda-forge::hdf5
+./venv/bin/python ./setup.py sdist bdist_wheel
 
 echo "Done!"
