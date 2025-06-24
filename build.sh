8941e1a721540de28993861fa0dadfc1e70fcdb8
tobias ferreira <soutobias@yahoo.com.br>
2025-06-24 11:54:25 +0100

    update build package workflow

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")"  # Set working directory
 
 mkdir -p build dist
 echo "Cleaning up old versions..."
-./venv/bin/pip install Cython
+./venv/bin/pip install "cython<3.0.0"
 ./venv/bin/pip install --no-build-isolation PyYAML==6.0
 rm -rf ./build/* ./dist/* ./Example_Python_Package.egg-info
 echo "Building package for distribution..."
