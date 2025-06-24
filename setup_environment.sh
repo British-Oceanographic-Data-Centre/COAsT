e82e6836aef490582315f92e7b68bf0ec83704b2
Magda Juarez <magjua@noc.ac.uk>
2025-06-24 18:12:44 +0100

    Update setup_environment.sh
    Update script to intialise conda in a shell session

--- setup_environment.sh
+++ setup_environment.sh
@@ -6,6 +6,8 @@ cd "$(dirname "$0")"  # Set working directory
 echo "Cleaning up any existing virtual environment..."
 rm -rf ./venv
 echo "Installing required packages..."
+CONDA_BASE=$(conda info --base)
+source "${CONDA_BASE}/etc/profile.d/conda.sh"
 conda activate coast
 conda install conda-forge::setuptools conda-forge::wheel conda-forge::twine
 
